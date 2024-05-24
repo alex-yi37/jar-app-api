f7ea985ff3ee355203800e7124e023aafef28794
alex <alexanderyi933@gmail.com>
2024-05-23 23:58:52 -0400

    creates has_many association on User model to select Jars that they own

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
+
+  # association to indicate a User has many Jars through the Jar table itself
+  has_many :owned_jars, class_name: 'Jar'
+
+  # SO thread for listing :belongs_to associations
+  # https://stackoverflow.com/questions/8371574/is-there-a-way-to-list-all-belongs-to-associations
+  # use User.reflect_on_all_associations(:belongs_to)
+
   validates :username, :email, :display_name, presence: true
 end
