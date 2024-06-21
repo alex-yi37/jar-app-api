ef29b9b9bf803eee7912a1c325ddd74b9f4be99f
alex <alexanderyi933@gmail.com>
2024-06-21 16:54:46 -0400

    rename password_hash column to password_digest on users table for use with has_secure_password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_05_232618) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_21_202947) do
   create_table "jar_invites", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
@@ -55,7 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_05_232618) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password_hash"
+    t.string "password_digest"
     t.string "display_name"
     t.date "birthday"
     t.datetime "created_at", null: false
